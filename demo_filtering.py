aa413ed720cd5cdd199e90c5dcea25aca9c4854d
vinhbui107 <vinhbui.tg@gmail.com>
2020-03-06 20:47:39 +0700

    add data folder

--- demo_filtering.py
+++ demo_filtering.py
@@ -1,3 +1,12 @@
 import pandas as pd
 import numpy as np
 
+# load du lieu tu file csv
+
+# vector hoa du lieu thong tin nguoi dung
+
+# chuan hoa du lieu danh gia cua nguoi dung
+
+# tinh toan su tuong dong thong tin nguoi dung
+
+# Rating Prediction
